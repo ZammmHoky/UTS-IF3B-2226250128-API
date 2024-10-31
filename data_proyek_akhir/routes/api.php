fe16b9d123a81f03e07f1402e7480d2761115c19
ZammmHoky <choirunizam1099@gmail.com>
2024-10-31 14:53:14 +0700

    tes

--- data_proyek_akhir/routes/api.php
+++ data_proyek_akhir/routes/api.php
@@ -1,4 +1,11 @@
 <?php
 
-Route::get('/api/projects', 'ProjectController@index');
-Route::post('/api/projects', 'ProjectController@store');
+use App\Http\Controllers\ProjectController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::post('/projects', [ProjectController::class, 'store']);
+
+Route::get('/projects/{project}', [ProjectController::class, 'show']);
+Route::post('/projects/{project}/update', [ProjectController::class, 'update']);
